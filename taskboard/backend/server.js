74604b944f51b1063c8da418a20a80a899a8af1b
Divya Shinde <divya73908@gmail.com>
2025-09-24 22:27:01 +0530

    adding deployment

--- taskboard/backend/server.js
+++ taskboard/backend/server.js
@@ -29,7 +29,7 @@ app.use('/api/tasks', taskRouter);
 const __dirname1 = path.resolve();
 if(process.env.NODE_ENV === 'deployment'){
     app.use(express.static(path.join(__dirname1, 'client', 'dist')));
-    app.get("/", (req, res) => {
+    app.get(/.*/, (req, res) => {
         res.sendFile(path.resolve(__dirname1, 'client', 'dist', 'index.html'));
     });
 }
